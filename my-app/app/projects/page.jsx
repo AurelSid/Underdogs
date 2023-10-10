c830949d72c9d19488bac5eba6dc1684602ed032
Aurélio <aurelsid021@gmail.com>
2023-10-10 14:12:05 +0200

    new commit

--- my-app/app/projects/page.jsx
+++ my-app/app/projects/page.jsx
@@ -12,6 +12,7 @@ const page = () => {
   const [projectId, setProjectId] = useState("");
   const [coverUrl, setCoverUrl] = useState("");
   const [galleryActive, setGalleryActive] = useState(false);
+  const [delay, setDelay] = useState(0.5);
 
   return (
     <div className="w-full h-full bg-[#d4d4d4] flex">
@@ -19,81 +20,44 @@ const page = () => {
         <div className="  w-full h-full justify-center grid grid-cols-2 items-center md:mx-20 mt-20">
           {portfolio.map((portfolio, index) => (
             <div className="" key={index}>
-              {index % 2 === 0 ? (
-                <motion.div
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 1 }}
-                  className=" bg-neutral-400 m-5 p-5 rounded-xl  justify-center  shadow-zinc-700 shadow-lg "
-                >
-                  {" "}
-                  <div className=" flex w-full">
-                    <img
-                      src={portfolio.cover}
-                      alt=""
-                      className="max-w-full max-h-full m-auto rounded-xl "
-                    />
-                  </div>
-                  <div className="md:mx-10 my-5">
-                    <h1 className=" text-left">
-                      <div className="text-xl  mb-5">
-                        {portfolio.projectname}
-                      </div>
-                      <div className="md:text-lg text-sm">
-                        {portfolio.description}
-                      </div>
-                      <button
-                        className="mt-3 underline"
-                        onClick={() => (
-                          setProjectId(index),
-                          setGalleryActive(true),
-                          setCoverUrl(portfolio.cover),
-                          console.log(index)
-                        )}
-                      >
-                        {"Gallery >"}
-                      </button>
-                    </h1>
-                  </div>
-                </motion.div>
-              ) : (
-                <motion.div
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 1 }}
-                  className=" bg-neutral-400 m-5 p-5 rounded-xl  justify-center shadow-zinc-700 shadow-lg "
-                >
-                  {" "}
-                  <div className=" flex w-full">
-                    <img
-                      src={portfolio.cover}
-                      alt=""
-                      className="max-w-full max-h-full m-auto rounded-xl "
-                    />
-                  </div>
-                  <div className="md:mx-10 my-5">
-                    <h1 className=" text-left">
-                      <div className="md:text-xl mb-5">
-                        {portfolio.projectname}
-                      </div>
-                      <div className="md:text-lg text-sm">
-                        {portfolio.description}
-                      </div>
-                      <button
-                        className="mt-3 underline"
-                        onClick={() => (
-                          setProjectId(index),
-                          setGalleryActive(true),
-                          setCoverUrl(portfolio.cover),
-                          console.log(index)
-                        )}
-                      >
-                        {"Gallery >"}{" "}
-                      </button>
-                    </h1>
-                  </div>
-                </motion.div>
-              )}
+              <motion.div
+                className=" bg-neutral-400 m-5 p-5 rounded-xl  justify-center  shadow-zinc-700 shadow-lg "
+                initial={{ x: -100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{
+                  ease: "easeOut",
+                  duration: 2,
+                  delay: delay + index * 0.4,
+                }}
+              >
+                {" "}
+                <div className=" flex w-full">
+                  <img
+                    src={portfolio.cover}
+                    alt=""
+                    className="max-w-full max-h-full m-auto rounded-xl "
+                  />
+                </div>
+                <div className="md:mx-10 my-5">
+                  <h1 className=" text-left">
+                    <div className="text-xl  mb-5">{portfolio.projectname}</div>
+                    <div className="md:text-lg text-sm">
+                      {portfolio.description}
+                    </div>
+                    <button
+                      className="mt-3 underline"
+                      onClick={() => (
+                        setProjectId(index),
+                        setGalleryActive(true),
+                        setCoverUrl(portfolio.cover),
+                        console.log(index)
+                      )}
+                    >
+                      {"Gallery >"}
+                    </button>
+                  </h1>
+                </div>
+              </motion.div>
             </div>
           ))}
         </div>
--- my-app/app/projects/portfolio.js
+++ my-app/app/projects/portfolio.js
@@ -2,73 +2,139 @@ const portfolio = [
   {
     projectname: "keyboard",
     description: "just a keyboard",
-    cover: "archi1.jpg",
+    cover: "./clavier/clavier2.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/chord.jpg" , "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg"]
+    galleryimage : [
+      "./clavier/1.jpg" ,
+      "./clavier/2.jpg" ,
+      "./clavier/3.jpg" ,
+      "./clavier/4.jpg" ,
+      "./clavier/5.jpg" ,
+      "./clavier/6.jpg" ,
+      "./clavier/7.jpg" ,
+      "./clavier/8.jpg" ,
+      "./clavier/9.jpg" ,
+      "./clavier/10.jpg" ,
+      "./clavier/11.jpg" ,
+   ]
   },
   {
-    projectname: "keyboard",
+    projectname: "Sutha",
     description: "just a keyboard",
-    cover: "sutha.jpg",
+    cover: "./sutha/sutha.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/chord.jpg" , "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg"]
+    galleryimage : [
+      "./sutha/8.jpg" ,
+      "./sutha/9.jpg" ,
+      "./sutha/1.jpg" ,
+      "./sutha/2.jpg" ,
+      "./sutha/3.jpg" ,
+      "./sutha/4.jpg" ,
+      "./sutha/6.jpg" ,
+      "./sutha/7.jpg" ,
+     "./sutha/10.jpg" ,
+     
+   ]
   },
   {
-    projectname: "keyboard",
+    projectname: "Archi",
     description: "just a keyboard",
-    cover: "gimmycover.jpg",
+    cover: "./house1/archi1.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/chord.jpg" , "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg"]
+    galleryimage : [
+      "./house1/8.jpg" ,
+      "./house1/9.jpg" ,
+      "./house1/1.jpg" ,
+      "./house1/2.jpg" ,
+      "./house1/3.jpg" ,
+      "./house1/4.jpg" ,
+      "./house1/6.jpg" ,
+      "./house1/7.jpg" ,
+     "./house1/10.jpg" ,
+     "./house1/11.jpg" ,
+     
+   ]
   },
   {
-    projectname: "keyboard",
+    projectname: "Gimmy",
     description: "just a keyboard",
-    cover: "bloomscover.jpg",
+    cover: "./gimmy/gimmycover.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/chord.jpg" , "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg"]
+    galleryimage : [
+  
+ 
+      "./gimmy/1.png" ,
+      "./gimmy/2.png" ,
+     
+      "./gimmy/3.jpg" ,
+      "./gimmy/gimmycover.jpg" ,
+     
+   
+     
+   ]
   },
   {
-    projectname: "keyboard",
+    projectname: "Chord",
     description: "just a keyboard",
-    cover: "/gallerypics/chord.jpg",
+    cover: "./chord/cover.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/chord.jpg" , "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg", "/gallerypics/chord.jpg"]
+    galleryimage : [
+      
+      "./chord/1.jpg" ,
+      "./chord/2.jpg" ,
+      "./chord/3.jpg" ,
+      "./chord/4.jpg" ,
+      "./chord/5.png" ,
+    
+     
+   ]
   },
   {
-    projectname: "keyboard",
+    projectname: "Blooms",
     description: "just a keyboard",
-    cover: "./clavier2.jpg",
+    cover: "./bloom/bloomscover.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/perottin.jpg" , "/gallerypics/chord.jpg"]
+    galleryimage : [
+      
+      "./bloom/1.jpg" ,
+      "./bloom/2.jpg" ,
+      "./bloom/3.jpg" ,
+      "./bloom/4.jpg" ,
+      "./bloom/5.jpg" ,
+      "./bloom/6.jpg" ,
+      "./bloom/7.jpg" ,
+    
+     
+   ]
   },
   {
-    projectname: "keyboard",
+    projectname: "Ezy Mobilier",
     description: "just a keyboard",
-    cover: "./chaise.png",
+    cover: "./chair/cover.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/perottin.jpg" , "/gallerypics/chord.jpg"]
+    galleryimage : [      
+    "./chair/1.jpg" ,
+    "./chair/2.jpg" ,
+    "./chair/3.jpg" ,
+    "./chair/4.jpg" ,
+    "./chair/5.jpg" ,
+    "./chair/6.jpg" ,]
   },
   {
-    projectname: "keyboard",
+    projectname: "D7RL Kustom kit",
     description: "just a keyboard",
-    cover: "./gal.png",
+    cover: "./rocketleague/cover.jpg",
     caption: "whatever",
-    galleryimage : ["/gallerypics/perottin.jpg" , "/gallerypics/chord.jpg"]
+    galleryimage : [      
+      "./rocketleague/1.jpg" ,
+      "./rocketleague/2.jpg" ,
+      "./rocketleague/3.jpg" ,
+      "./rocketleague/4.jpg" ,
+      "./rocketleague/5.jpg" ,
+      
+      "./rocketleague/cover.jpg" ,]
   },
-  {
-    projectname: "keyboard",
-    description: "just a keyboard",
-    cover: "./d7rcover.jpg",
-    caption: "whatever",
-    galleryimage : ["/gallerypics/perottin.jpg" , "/gallerypics/chord.jpg"]
-  },
-  {
-    projectname: "keyboard",
-    description: "just a keyboard",
-    cover: "./xx.png",
-    caption: "whatever",
-    galleryimage : ["/gallerypics/perottin.jpg" , "/gallerypics/chord.jpg"]
-  },
+
 ];
 
 export default portfolio;
